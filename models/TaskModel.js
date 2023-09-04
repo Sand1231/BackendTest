954dbe39848197aab1766718852a9b7d7267dc19
Ahmed Ali Ansari <ahmedaliansari506@gmail.com>
2023-09-04 18:31:49 +0500

    "task id issue resolved"

--- models/TaskModel.js
+++ models/TaskModel.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-  TaskID: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    unique: true,
-  },
   Title: {
     type: String,
     required: true,
